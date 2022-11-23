1cd6fa90bdb179c22732373545161efa0c17653e
Jmarkzcod8r <jmgutierrez122091@gmail.com>
2022-11-23 23:34:18 +0800

    first commit Django for railway

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn todo_app.wsgi --log-file -
+web: gunicorn mysite.wsgi --log-file -
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,8 @@ SECRET_KEY = 'django-insecure-dnuh=x$eq@kln)f8+&l#85u^au4z4xpi#$^1x9u)(cv*k)4q)u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# This means that any website is allowable
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
